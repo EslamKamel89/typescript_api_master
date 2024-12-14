3b926b444bdd8cc72557b1f3d72f92a290735427
EslamKamel89 <elsamkamelforex@gmail.com>
2024-12-14 10:58:15 +0200

    showing the posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import fetchingImg from "./assets/data-fetching.png";
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import { get } from "./util/http";
+type RawDataBlogPost = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPost() {
+      const data: RawDataBlogPost[] = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawDataBlogPost[];
+      const blogPosts: BlogPost[] = data.map((rawPost) => ({
+        id: rawPost.id,
+        title: rawPost.title,
+        text: rawPost.body,
+      }));
+      setFetchedPosts(blogPosts);
+    }
+    fetchPost();
+    return () => {};
+  }, []);
+  return (
+    <main>
+      <img src={fetchingImg} />
+      <BlogPosts posts={fetchedPosts ?? []}></BlogPosts>
+    </main>
+  );
 }
 
 export default App;
